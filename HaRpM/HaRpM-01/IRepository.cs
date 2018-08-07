d38cf5ce650650c633285d36bf8a010d76bb8be4
GEN <gaston_e_nusimovich@hotmail.com>
2018-08-06 20:58:46 -0400

    IRepository interface added

---
+++ HaRpM/HaRpM-01/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+public interface IRepository<T> where T : class, IEntity
+{
+       T FindBy(int id);
+       IList<T> FindAll();
+       void Add(T item);
+       void Remove(T item);
+       void Remove(int id);
+       void Update(T item);
+}
